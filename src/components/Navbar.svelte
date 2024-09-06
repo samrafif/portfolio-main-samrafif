b4edc0af1a1e0b1d0de46f2a1de552c8adfa0419
Samwoof <abrisam.rm@gmail.com>
2024-09-06 14:39:00 +0700

    Implemented Nav toggle

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,8 +1,12 @@
 <script>
   import LayoutFlexRow from "./LayoutFlexRow.svelte";
   import { location } from "svelte-spa-router";
+  import active from "svelte-spa-router/active";
+  import { writable } from "svelte/store";
+  import { fly } from "svelte/transition";
   export let paths;
   let paths_filtered = [];
+  let open = writable("");
 
   $: {
     paths_filtered = [];
@@ -16,19 +20,46 @@
     // console.log(paths_filtered);
     // console.log($location);
   }
+  function toggleNav() {
+    if ($open == "") {
+      open.set("open");
+    } else {
+      open.set("");
+    }
+    console.log($open);
+  }
 </script>
 
-<div class="component-navbar">
-  <LayoutFlexRow gap="16px">
+<div class="navbar-container">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click={toggleNav} class="burger">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
+  <div class="component-navbar {$open}">
     {#each paths_filtered as path, i}
+      <!-- TODO: Use svelte-spa-router's provided active method, rather than this crap -->
       <div class="navbar-nav-item {path.active}">
         <a href="#{path.path}">{path.text}</a>
       </div>
     {/each}
-  </LayoutFlexRow>
+  </div>
 </div>
 
 <style>
+  .burger {
+    display: none;
+  }
+  .bar {
+    background-color: white;
+    margin-bottom: 4px;
+    margin-right: 12px;
+    height: 5px;
+    width: 26px;
+  }
+
   .navbar-nav-item {
     padding: 4px 16px 4px 16px;
     transition: all 500ms;
@@ -46,5 +77,33 @@
 
   .active a {
     color: var(--bg-color) !important;
+  }
+
+  .component-navbar {
+    z-index: 1;
+    display: flex;
+    gap: 16px;
+    transition: width 1s;
+    background-color: var(--bg-color);
+  }
+
+  @media screen and (max-width: 850px) {
+    .component-navbar {
+      left: 0;
+      position: absolute;
+      display: flex;
+      flex-direction: column;
+      display: none;
+    }
+
+    .burger {
+      display: block;
+    }
+  }
+
+  .open {
+    top: 10%;
+    width: 100%;
+    display: block;
   }
 </style>
