554ec63dcd4e399f7348e26fc1f033251ecf3267
Samwoof <abrisam.rm@gmail.com>
2024-09-06 14:37:38 +0700

    Implemented Contact UI

---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,42 @@
+<script>
+  import LayoutFlexColumn from "./LayoutFlexColumn.svelte";
+</script>
+
+<main>
+  <LayoutFlexColumn gap="10px">
+    <input placeholder="Ya name" type="text" />
+    <input
+      placeholder="Ya email (so like I can slide in ur inbox)"
+      type="email"
+    />
+    <input placeholder="Ya website" type="text" />
+    <textarea placeholder="Whaddya wanna talk bout :3" name="message" id=""
+    ></textarea>
+    <br />
+    <button>Touch me.</button>
+  </LayoutFlexColumn>
+</main>
+
+<style>
+  main {
+    width: 40%;
+  }
+  button {
+    background-color: var(--text-color);
+    color: var(--bg-color);
+    padding: 1rem;
+  }
+  input {
+    padding: 1rem;
+    transition: all 1s;
+  }
+  textarea {
+    padding: 1rem 1rem 2rem;
+  }
+
+  @media screen and (max-width: 850px) {
+    main {
+      width: 85%;
+    }
+  }
+</style>
--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,12 +1,57 @@
 <script>
   import { OnMount } from "fractils";
   import { fly } from "svelte/transition";
+  import ContactForm from "../components/ContactForm.svelte";
+  import LayoutFlexRow from "../components/LayoutFlexRow.svelte";
+  import LayoutFlexColumn from "../components/LayoutFlexColumn.svelte";
 </script>
 
 <main>
   <OnMount>
-    <div in:fly={{ y: -50, duration: 1000 }}>
+    <div class="contact-container" in:fly={{ y: -50, duration: 1000 }}>
       <h1>Contact</h1>
+      <br />
+      <!-- TODO: Use fancy stores for this rather than repetitive CSS  -->
+      <!-- TODO: Standardize class names  -->
+      <div class="contact-body-container">
+        <ContactForm />
+        <LayoutFlexColumn>
+          <h1>Lets talk.. like for a job</h1>
+          <p>
+            I will get your project done, buuuuuuuut...
+            <br />
+            I'll give what you paid for sooo ehem pay me lots baby
+          </p>
+          <br />
+          <p><b>Find more of me here</b></p>
+          <LayoutFlexRow>
+            <!-- TODO: Extract to component -->
+            <button>GH</button>
+            <button>IG</button>
+            <button>LN</button>
+            <button>BC</button>
+            <button>DC</button>
+          </LayoutFlexRow>
+        </LayoutFlexColumn>
+      </div>
     </div>
   </OnMount>
 </main>
+
+<style>
+  p {
+    margin: 0;
+  }
+
+  .contact-body-container {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+  }
+
+  @media screen and (max-width: 850px) {
+    .contact-body-container {
+      flex-direction: column;
+    }
+  }
+</style>
